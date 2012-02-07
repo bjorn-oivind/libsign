4b3fc772a15fc43ae6175ede9811195e12067d31
Bjørn Øivind Bjørnsen <Bjorn.Oivind.Bjornsen@jotron.com>
2012-02-07 10:34:49 +0100

    armor: test for both unix and windows line endings.

--- src/armor.c
+++ src/armor.c
@@ -24,9 +24,12 @@ int decode_armor(const uint8_t *armor_in, uint32_t armor_len, uint8_t **plain_ou
       - the armor tail */
 
     /* find the start of the armor */
-    i = 1;
+    i = 3;
     while(i++ < armor_len-1) {
-        if(armor_in[i] == '\n' && armor_in[i-1] == '\n')
+        /* test for both unix style endings and windows style */
+        if((armor_in[i-3] == '\n' && armor_in[i-2] == '\n') ||
+           (armor_in[i-3] == '\r' && armor_in[i-2] == '\n' && 
+            armor_in[i-1] == '\r' && armor_in[i] == '\n'))
             break;
     }
     /* did we find it? */
