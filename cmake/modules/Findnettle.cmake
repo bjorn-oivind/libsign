4fcb999c35dc7049b9d72e02eebb40942480babc
Bjørn Øivind Bjørnsen <Bjorn.Oivind.Bjornsen@jotron.com>
2012-03-21 12:33:06 +0100

    cmake: search for the GMP include dir too.
    This also fixes a typo which caused the find_* commands to fail on windows.

--- cmake/modules/Findnettle.cmake
+++ cmake/modules/Findnettle.cmake
@@ -7,12 +7,12 @@
 
 find_path(Nettle_INCLUDE_DIR NAMES nettle-types.h
           PATH_SUFFIXES nettle
-          PATH "$ENV{PROGRAMFILES}/nettle/include"
+          PATHS "$ENV{PROGRAMFILES}/nettle/include"
           DOC "The Nettle include directory"
 )
 
 find_library(Nettle_LIBRARY NAMES nettle
-             PATH "$ENV{PROGRAMFILES}/nettle/lib"
+             PATHS "$ENV{PROGRAMFILES}/nettle/lib"
              DOC "The nettle library"
 )
 
@@ -20,19 +20,27 @@ find_library(Nettle_LIBRARY NAMES nettle
 # if we are on win32, we need to find mpir instead of gmp, hogweed is compiled into nettle.
 if(WIN32)
 
+  find_path(Gmp_INCLUDE_DIR NAMES gmp.h
+            PATHS "$ENV{PROGRAMFILES}/mpir/include"
+			DOC "The MPIR include directory"
+  )
+
   find_library(Gmp_LIBRARY NAMES mpir
-               PATH "$ENV{PROGRAMFILES}/mpir/lib"
+               PATHS "$ENV{PROGRAMFILES}/mpir/lib"
                DOC "Nettle dependency mpir"
   )
 
 else(WIN32)
 
+  find_path(Gmp_INCLUDE_DIR NAMES gmp.h
+            DOC "The GMP include directory")
+
   find_library(Gmp_LIBRARY NAMES gmp
                DOC "Nettle dependency gmp"
   )
 
-find_library(Hogweed_LIBRARY NAMES hogweed
-             DOC "Hogweed library from nettle"
+  find_library(Hogweed_LIBRARY NAMES hogweed
+               DOC "Hogweed library from nettle"
   )
 
 endif()
@@ -41,19 +49,19 @@ endif()
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
 if(WIN32)
-  find_package_handle_standard_args(Nettle DEFAULT_MSG Nettle_LIBRARY Nettle_INCLUDE_DIR Gmp_LIBRARY)
+  find_package_handle_standard_args(Nettle DEFAULT_MSG Nettle_LIBRARY Nettle_INCLUDE_DIR Gmp_LIBRARY Gmp_INCLUDE_DIR)
 else(WIN32)
-  find_package_handle_standard_args(Nettle DEFAULT_MSG Nettle_LIBRARY Nettle_INCLUDE_DIR Hogweed_LIBRARY Gmp_LIBRARY)
+  find_package_handle_standard_args(Nettle DEFAULT_MSG Nettle_LIBRARY Nettle_INCLUDE_DIR Hogweed_LIBRARY Gmp_LIBRARY Gmp_INCLUDE_DIR)
 endif()
 
 set(Nettle_FOUND ${NETTLE_FOUND})
 unset(NETTLE_FOUND)
 
 if(Nettle_FOUND)
-  set(Nettle_LIBRARIES ${Nettle_LIBRARY} ${Hogweed_LIBRARY} ${Hogweed_LIBRARY} ${Gmp_LIBRARY})
-  set(Nettle_INCLUDE_DIRS ${Nettle_INCLUDE_DIR})
+  set(Nettle_LIBRARIES ${Nettle_LIBRARY} ${Hogweed_LIBRARY} ${Hogweed_LIBRARY} ${Gmp_LIBRARY} CACHE STRING "Libraries used to link nettle")
+  set(Nettle_INCLUDE_DIRS ${Nettle_INCLUDE_DIR} ${Gmp_INCLUDE_DIR} CACHE STRING "Nettle include directories")
 endif()
 
-mark_as_advanced(Nettle_INCLUDE_DIR Nettle_LIBRARY Hogweed_LIBRARY Gmp_LIBRARY)
+mark_as_advanced(Nettle_INCLUDE_DIR Nettle_LIBRARY Hogweed_LIBRARY Gmp_LIBRARY Gmp_INCLUDE_DIR)
 
 # vi: set ts=2 shiftwidth=2 expandtab :
