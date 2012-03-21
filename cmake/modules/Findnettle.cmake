4da2cde494816f964deecad70315951a96450daf
Bjørn Øivind Bjørnsen <Bjorn.Oivind.Bjornsen@jotron.com>
2012-03-21 11:11:19 +0100

    libsign: refactor findNettle.cmake.

--- cmake/modules/Findnettle.cmake
+++ cmake/modules/Findnettle.cmake
@@ -1,102 +1,59 @@
-# Find nettle
+# - Find nettle libraries and headers.
+# This module defines the following variables:
 #
-# This module defines
-#  NETTLE_FOUND - whether the nettle library and its dependecies were found
-#  NETTLE_LIBRARIES - the nettle library and its dependencies
-#  NETTLE_INCLUDE_DIR - the include paths of the nettle library and its dependencies
-#  NETTLE_LIBRARY_DIR - the paths of the nettle library and its dependencies
-#  NETTLE_LIBRARY_DLL - the absolute path to the library on windows (only set on WIN32).
-#
+# Nettle_FOUND          - true if nettle was found.
+# Nettle_INCLUDE_DIRS   - include search path
+# Nettle_LIBRARIES      - libraries to link
 
-if (NETTLE_INCLUDE_DIR AND NETTLE_LIBRARIES AND NETTLE_LIBRARY_DIR)
+find_path(Nettle_INCLUDE_DIR NAMES nettle-types.h
+          PATH_SUFFIXES nettle
+          PATH "$ENV{PROGRAMFILES}/nettle/include"
+          DOC "The Nettle include directory"
+)
 
-  # Already in cache
-  set (NETTLE_FOUND TRUE)
+find_library(Nettle_LIBRARY NAMES nettle
+             PATH "$ENV{PROGRAMFILES}/nettle/lib"
+             DOC "The nettle library"
+)
 
-else (NETTLE_INCLUDE_DIR AND NETTLE_LIBRARIES AND NETTLE_LIBRARY_DIR)
+# if we are on *nix, we need to find gmp and hogweed.
+# if we are on win32, we need to find mpir instead of gmp, hogweed is compiled into nettle.
+if(WIN32)
 
-  if (NOT WIN32)
-    set(NETTLE_LIBRARY_DIR "/usr/lib")
-    set(NETTLE_INCLUDE_DIR "/usr/include/nettle")
-  else(NOT WIN32)
-    file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}/nettle/lib" NETTLE_LIBRARY_DIR)
-    file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}/nettle/include" NETTLE_INCLUDE_DIR)
-	file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}/mpir/lib" MPIR_LIBRARY_DIR)
-	file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}/mpir/include" MPIR_INCLUDE_DIR)	
-  endif (NOT WIN32)
-
-  find_library (NETTLE_LIBRARIES
-    NAMES
-    nettle
-    PATHS
-    ${NETTLE_LIBRARY_DIR}
-    ${LIB_INSTALL_DIR}
+  find_library(Gmp_LIBRARY NAMES mpir
+               PATH "$ENV{PROGRAMFILES}/mpir/lib"
+               DOC "Nettle dependency mpir"
   )
 
-  find_path (NETTLE_INCLUDE_DIR
-    NAMES
-    nettle-types.h
-    PATHS
-    ${NETTLE_INCLUDE_DIR}
-    ${INCLUDE_INSTALL_DIR}
+else(WIN32)
+
+  find_library(Gmp_LIBRARY NAMES gmp
+               DOC "Nettle dependency gmp"
   )
 
-  if(WIN32)
-    if(MINGW)
-	  file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}/nettle/bin/libnettle.dll" NETTLE_LIBRARY_DLL)
-    elseif(MSVC)
-	  file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}/nettle/bin/nettle.dll" NETTLE_LIBRARY_DLL)
-    endif(MINGW)
-	
-	# We need to find the mpir dependency as well
-    find_library (MPIR_LIBRARY
-      NAMES
-      mpir
-      PATHS
-      ${MPIR_LIBRARY_DIR}
-      ${LIB_INSTALL_DIR}
-    )
-	
-	if(NOT MPIR_LIBRARY)
-	    Message(FATAL_ERROR "Nettle dependency mpir not found.")
-	endif(NOT MPIR_LIBRARY)
-	
-	list(APPEND NETTLE_LIBRARIES ${MPIR_LIBRARY})
-	list(APPEND NETTLE_INCLUDE_DIR ${MPIR_INCLUDE_DIR})
-	
-    set(NETTLE_FOUND 1)
-  else(WIN32)
-	# We require hogweed as well as gmp on Linux
-	find_library (GMP_LIBRARY
-		NAMES
-		gmp
-		PATHS
-		${NETTLE_LIBRARY_DIR}
-		${LIB_INSTALL_DIR}
-	)
-	
-	if(NOT GMP_LIBRARY)
-		Message(FATAL_ERROR "Nettle dependency gmp not found.")
-	endif(NOT GMP_LIBRARY)
-	
-	find_library (HOGWEED_LIBRARY
-		NAMES
-		hogweed
-		PATHS
-		${NETTLE_LIBRARY_DIR}
-		${LIB_INSTALL_DIR}
-	)
-	
-	if(NOT HOGWEED_LIBRARY)
-		Message(FATAL_ERROR "Nettle dependency hogweed not found.")
-	endif(NOT HOGWEED_LIBRARY)
-	
-	list(APPEND NETTLE_LIBRARIES ${GMP_LIBRARY})
-	list(APPEND NETTLE_LIBRARIES ${HOGWEED_LIBRARY})
-	set(NETTLE_FOUND 1)
-  endif(WIN32)
+find_library(Hogweed_LIBRARY NAMES hogweed
+             DOC "Hogweed library from nettle"
+  )
 
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(Nettle DEFAULT_MSG NETTLE_LIBRARIES NETTLE_INCLUDE_DIR NETTLE_LIBRARY_DIR)
+endif()
 
-endif (NETTLE_INCLUDE_DIR AND NETTLE_LIBRARIES AND NETTLE_LIBRARY_DIR)
+# handle the QUIETLY and REQUIRED arguments and set NETTLE_FOUND to TRUE if
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+if(WIN32)
+  find_package_handle_standard_args(Nettle DEFAULT_MSG Nettle_LIBRARY Nettle_INCLUDE_DIR Gmp_LIBRARY)
+else(WIN32)
+  find_package_handle_standard_args(Nettle DEFAULT_MSG Nettle_LIBRARY Nettle_INCLUDE_DIR Hogweed_LIBRARY Gmp_LIBRARY)
+endif()
+
+set(Nettle_FOUND ${NETTLE_FOUND})
+unset(NETTLE_FOUND)
+
+if(Nettle_FOUND)
+  set(Nettle_LIBRARIES ${Nettle_LIBRARY} ${Hogweed_LIBRARY} ${Hogweed_LIBRARY} ${Gmp_LIBRARY})
+  set(Nettle_INCLUDE_DIRS ${Nettle_INCLUDE_DIR})
+endif()
+
+mark_as_advanced(Nettle_INCLUDE_DIR Nettle_LIBRARY Hogweed_LIBRARY Gmp_LIBRARY)
+
+# vi: set ts=2 shiftwidth=2 expandtab :
