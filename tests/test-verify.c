b2bbb6e9c97b0f957e379ce245990cc3ce100819
Knut Andre Tidemann <Knut.Andre.Tidemann@jotron.com>
2013-08-13 10:11:20 +0200

    tests: add testing of data integrity after verify.
    [Removed unused variable and replaced loop with memcmp() - BØB]

--- tests/test-verify.c
+++ tests/test-verify.c
@@ -2,9 +2,18 @@
 #include "signature.h"
 #include "public_key.h"
 
+#include <stdio.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main()
 {
     int ret;
+    FILE *fp = NULL;
+    struct stat st;
+    uint8_t *data = NULL, *data_backup = NULL;
 
     libsign_signature sig;
     libsign_public_key pub;
@@ -21,10 +30,46 @@ int main()
         goto exit;
 
     ret = verify(&pub, &sig, "files/vmImage");
+    if(ret < 0)
+        goto exit;
 
+    ret = stat("files/vmImage", &st);
+    if(ret < 0)
+        goto exit;
+
+    data = malloc(st.st_size);
+    data_backup = malloc(st.st_size);
+    if(!data) {
+        ret = -errno;
+        goto exit;
+    }
+
+    fp = fopen("files/vmImage", "r");
+    if(!fp) {
+        ret = -errno;
+        goto exit;
+    }
+
+    if(fread(data, sizeof(uint8_t), st.st_size, fp) != (size_t)st.st_size) {
+        ret = -1;
+        goto exit;
+    }
+
+    memcpy(data_backup, data, st.st_size);
+
+    ret = verify_buffer(&pub, &sig, data, (uint32_t)st.st_size);
+    if(ret < 0)
+        goto exit;
+
+    ret = memcmp(data, data_backup, st.st_size) != 0;
 exit:
+    if(fp)
+        fclose(fp);
+
     signature_destroy(&sig);
     public_key_destroy(&pub);
+    free(data);
+    free(data_backup);
 
     return ret;
 }
